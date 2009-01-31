e99d5249e459f794f0bfb58bc7a66e82c1cf29ff
nbd <nbd@3c298f89-4303-0410-b956-a3cf2f4a3e73>
2009-01-31 18:47:12 +0000

    branch packages for 8.09

--- target/linux/ixp4xx/generic/target.mk
+++
@@ -1,9 +0,0 @@
-BOARDNAME:=Generic
-
-DEFAULT_PACKAGES+= hostapd-mini
-
-define Target/Description
-	Build firmware images for ixp4xx based boards that boot from internal flash
-	(e.g : Linksys NSLU2, ...)
-endef
-
--- target/linux/ps3/petitboot/target.mk
+++
@@ -1,5 +0,0 @@
-BOARDNAME:=Petitboot
-
-define Target/Description
-	Build Petitboot bootloader
-endef
